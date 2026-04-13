fe8ad6f2aab05115b0efd52bed4883fcc9885f5c
EddumuPushkar <eddumupushkar13@gmail.com>
2026-04-13 10:24:11 +0530

    addedurl

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:8080"
+      "/api": "https://binkeyit-ww2a.onrender.com"
     },
   },
   plugins: [react()],
